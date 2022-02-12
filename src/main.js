3d6d9561fead339e46d260e884fc904f044b5a27
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-02-12 12:39:46 +0100

    push basics

--- src/main.js
+++ src/main.js
@@ -1,17 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from "vue-router"
 
-const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-]
-
-const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes, // short for `routes: routes`
-})
-const app = Vue.createApp({})
-app.use(router);
-app.mount('#app')
+createApp(App).mount('#app')
